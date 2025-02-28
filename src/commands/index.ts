e8c966170d56898935a06f0b59f2a54e32769e2e
Rahul <chattychidiya@gmail.com>
2025-02-28 22:20:23 +0530

    feat: add deleteProject command and refresh UI after project operations

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode'
 import { MetricsDatabase } from '../DB/MetricsDatabase'
 import { DevelopmentActivityMonitor } from '../core/DevelopmentActivityMonitor'
+import { ProjectTreeItem } from '../views/ProjectsTreeProvider'
 import { changeProjectFolder } from './changeProjectFolder'
 import { createProject } from './createProject'
+import { deleteProject } from './deleteProject'
 import { manageProjects } from './manageProjects'
 import { renameProject } from './renameProject'
 import { toggleProjectTracking } from './toggleProjectTracking'
@@ -15,109 +17,215 @@ export async function registerCommands(
 ): Promise<vscode.Disposable[]> {
     const createProjectCmd = vscode.commands.registerCommand(
         'devmetrics.createProject',
-        () => createProject(db, monitors, globalStoragePath)
+        () =>
+            createProject(db, monitors, globalStoragePath).then(() => {
+                vscode.commands.executeCommand('devmetrics.refreshData')
+            })
+    )
+
+    const deleteProjectCmd = vscode.commands.registerCommand(
+        'devmetrics.deleteProject',
+        async (treeItem?: ProjectTreeItem) => {
+            if (treeItem && treeItem.project) {
+                // Called from tree view (Fetch project name from tree item)
+                const projectName = treeItem.project.name
+                await deleteProject(projectName, db, monitors)
+                vscode.commands.executeCommand('devmetrics.refreshData')
+                return
+            } else {
+                // Get all projects
+                const projects = await db.getAllProjects()
+
+                if (projects.length === 0) {
+                    vscode.window.showInformationMessage(
+                        'No projects available. Please create a project first.'
+                    )
+                    return
+                }
+
+                // Build project list
+                const projectItems = projects.map((p) => ({
+                    label: p.name,
+                    description: p.folder_path,
+                    project: p,
+                }))
+
+                // Let user select a project
+                const selected = await vscode.window.showQuickPick(
+                    projectItems,
+                    {
+                        placeHolder: 'Select a project to delete',
+                        title: 'Delete Project',
+                    }
+                )
+
+                if (!selected) {
+                    return // User cancelled
+                }
+
+                const projectName = selected.project.name
+                deleteProject(projectName, db, monitors).then(() =>
+                    vscode.commands.executeCommand('devmetrics.refreshData')
+                )
+            }
+        }
     )
 
     const manageProjectsCmd = vscode.commands.registerCommand(
         'devmetrics.manageProjects',
-        () => manageProjects(db, monitors, globalStoragePath)
+        () =>
+            manageProjects(db, monitors, globalStoragePath).then(() => {
+                vscode.commands.executeCommand('devmetrics.refreshData')
+            })
     )
 
     const renameProjectCmd = vscode.commands.registerCommand(
         'devmetrics.renameProject',
-        () => renameProject(db)
+        () =>
+            renameProject(db).then(() => {
+                vscode.commands.executeCommand('devmetrics.refreshData')
+            })
     )
 
     const changeProjectFolderCmd = vscode.commands.registerCommand(
         'devmetrics.changeProjectFolder',
-        () => changeProjectFolder(db, monitors)
+        () =>
+            changeProjectFolder(db, monitors).then(() => {
+                vscode.commands.executeCommand('devmetrics.refreshData')
+            })
     )
 
     const startTrackingCmd = vscode.commands.registerCommand(
         'devmetrics.startTracking',
-        async () => {
-            // Get all projects
-            const projects = await db.getAllProjects()
-
-            if (projects.length === 0) {
-                vscode.window.showInformationMessage(
-                    'No projects available. Please create a project first.'
+        async (treeItem?: ProjectTreeItem) => {
+            if (treeItem && treeItem.project) {
+                // Called from tree view (Fetch project name from tree item)
+                const projectName = treeItem.project.name
+                await toggleProjectTracking(
+                    projectName,
+                    true,
+                    db,
+                    monitors,
+                    globalStoragePath
                 )
+                vscode.commands.executeCommand('devmetrics.refreshData')
                 return
+            } else {
+                // Get all projects
+                const projects = await db.getAllProjects()
+
+                if (projects.length === 0) {
+                    vscode.window.showInformationMessage(
+                        'No projects available. Please create a project first.'
+                    )
+                    return
+                }
+
+                // Build project list
+                const projectItems = projects.map((p) => ({
+                    label: p.name,
+                    description: p.folder_path,
+                    project: p,
+                }))
+
+                // Let user select a project
+                const selected = await vscode.window.showQuickPick(
+                    projectItems,
+                    {
+                        placeHolder: 'Select a project to start tracking',
+                        title: 'Start Tracking',
+                    }
+                )
+
+                if (!selected) {
+                    return // User cancelled
+                }
+
+                const projectName = selected.project.name
+                toggleProjectTracking(
+                    projectName,
+                    true,
+                    db,
+                    monitors,
+                    globalStoragePath
+                ).then(() =>
+                    vscode.commands.executeCommand('devmetrics.refreshData')
+                )
             }
-
-            // Build project list
-            const projectItems = projects.map((p) => ({
-                label: p.name,
-                description: p.folder_path,
-                project: p,
-            }))
-
-            // Let user select a project
-            const selected = await vscode.window.showQuickPick(projectItems, {
-                placeHolder: 'Select a project to start tracking',
-                title: 'Start Tracking',
-            })
-
-            if (!selected) {
-                return // User cancelled
-            }
-
-            const projectName = selected.project.name
-            toggleProjectTracking(
-                projectName,
-                true,
-                db,
-                monitors,
-                globalStoragePath
-            )
         }
     )
 
     const stopTrackingCmd = vscode.commands.registerCommand(
         'devmetrics.stopTracking',
-        async () => {
-            // Get all projects
-            const projects = await db.getAllProjects()
-
-            if (projects.length === 0) {
-                vscode.window.showInformationMessage(
-                    'No projects available. Please create a project first.'
+        async (treeItem?: ProjectTreeItem) => {
+            if (treeItem && treeItem.project) {
+                // Called from tree view (Fetch project name from tree item)
+                const projectName = treeItem.project.name
+                await toggleProjectTracking(
+                    projectName,
+                    false,
+                    db,
+                    monitors,
+                    globalStoragePath
                 )
+                vscode.commands.executeCommand('devmetrics.refreshData')
                 return
+            } else {
+                // Get all projects
+                const projects = await db.getAllProjects()
+
+                if (projects.length === 0) {
+                    vscode.window.showInformationMessage(
+                        'No projects available. Please create a project first.'
+                    )
+                    return
+                }
+
+                // Build project list
+                const projectItems = projects.map((p) => ({
+                    label: p.name,
+                    description: p.folder_path,
+                    project: p,
+                }))
+
+                // Let user select a project
+                const selected = await vscode.window.showQuickPick(
+                    projectItems,
+                    {
+                        placeHolder: 'Select a project to stop tracking',
+                        title: 'Stop Tracking',
+                    }
+                )
+
+                if (!selected) {
+                    return // User cancelled
+                }
+
+                const projectName = selected.project.name
+                toggleProjectTracking(
+                    projectName,
+                    false,
+                    db,
+                    monitors,
+                    globalStoragePath
+                ).then(() =>
+                    vscode.commands.executeCommand('devmetrics.refreshData')
+                )
             }
-
-            // Build project list
-            const projectItems = projects.map((p) => ({
-                label: p.name,
-                description: p.folder_path,
-                project: p,
-            }))
-
-            // Let user select a project
-            const selected = await vscode.window.showQuickPick(projectItems, {
-                placeHolder: 'Select a project to stop tracking',
-                title: 'Stop Tracking',
-            })
-
-            if (!selected) {
-                return // User cancelled
-            }
-
-            const projectName = selected.project.name
-            toggleProjectTracking(
-                projectName,
-                false,
-                db,
-                monitors,
-                globalStoragePath
-            )
         }
     )
 
     const viewProjectMetricsCmd = vscode.commands.registerCommand(
         'devmetrics.viewProjectMetrics',
-        async () => {
+        async (treeItem?: any) => {
+            if (treeItem && treeItem.project) {
+                // Called from tree view
+                const projectName = treeItem.project.name
+                await viewProjectMetrics(projectName, db)
+                vscode.commands.executeCommand('devmetrics.refreshData')
+                return
+            }
+
             // Get all projects
             const projects = await db.getAllProjects()
 
@@ -146,12 +254,15 @@ export async function registerCommands(
             }
 
             const projectName = selected.project.name
-            viewProjectMetrics(projectName, db)
+            viewProjectMetrics(projectName, db).then(() =>
+                vscode.commands.executeCommand('devmetrics.refreshData')
+            )
         }
     )
 
     return [
         createProjectCmd,
+        deleteProjectCmd,
         manageProjectsCmd,
         renameProjectCmd,
         changeProjectFolderCmd,
